ee8f5e0133c8565d0ac99be79a18f38fd9a93dd5
Ivan Shevchenko <rayserviz@gmail.com>
2020-02-10 22:57:45 +0200

    Updates

---
+++ precache-manifest.5695a12aae7d200af54a8ef1f81ca8f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e08edfac8368bcf1340d9b1584674a0",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "f9c68e3358eca9d4f928",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.68ad9181.chunk.css"
+  },
+  {
+    "revision": "1a52373c893341943d33",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c59564cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c59564cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9c68e3358eca9d4f928",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.10ac0be1.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  }
+]);
